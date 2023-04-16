d18ce14059f3635a1b4a4de042c7bf7861c9a82e
Joca02 <jocaabramovic@gmail.com>
2023-04-16 17:33:11 +0200

    Fixovao sam bagove sa enemies, i pretvorio sam ih u dinamicke liste

--- SPACE_SHOOTER/Form1.Designer.cs
+++ SPACE_SHOOTER/Form1.Designer.cs
@@ -35,57 +35,64 @@
             this.tmrEnemies4 = new System.Windows.Forms.Timer(this.components);
             this.tmrEnemies5 = new System.Windows.Forms.Timer(this.components);
             this.tmr_my_bullets = new System.Windows.Forms.Timer(this.components);
+            this.lblNoBullets = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tmrEnemies
             // 
             this.tmrEnemies.Enabled = true;
-            this.tmrEnemies.Interval = 20;
             this.tmrEnemies.Tick += new System.EventHandler(this.tmrEnemies_Tick);
             // 
             // tmrEnemies2
             // 
             this.tmrEnemies2.Enabled = true;
-            this.tmrEnemies2.Interval = 20;
             this.tmrEnemies2.Tick += new System.EventHandler(this.tmrEnemies2_Tick);
             // 
             // tmrEnemies3
             // 
             this.tmrEnemies3.Enabled = true;
-            this.tmrEnemies3.Interval = 20;
             this.tmrEnemies3.Tick += new System.EventHandler(this.tmrEnemies3_tick);
             // 
             // tmrEnemies4
             // 
             this.tmrEnemies4.Enabled = true;
-            this.tmrEnemies4.Interval = 20;
             this.tmrEnemies4.Tick += new System.EventHandler(this.tmrEnemies4_Tick);
             // 
             // tmrEnemies5
             // 
             this.tmrEnemies5.Enabled = true;
-            this.tmrEnemies5.Interval = 20;
             this.tmrEnemies5.Tick += new System.EventHandler(this.tmrEnemies5_Tick);
             // 
             // tmr_my_bullets
             // 
             this.tmr_my_bullets.Enabled = true;
-            this.tmr_my_bullets.Interval = 1;
             this.tmr_my_bullets.Tick += new System.EventHandler(this.tmr_my_bullets_Tick);
+            // 
+            // lblNoBullets
+            // 
+            this.lblNoBullets.AutoSize = true;
+            this.lblNoBullets.BackColor = System.Drawing.Color.Transparent;
+            this.lblNoBullets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNoBullets.ForeColor = System.Drawing.Color.Red;
+            this.lblNoBullets.Location = new System.Drawing.Point(326, 434);
+            this.lblNoBullets.Name = "lblNoBullets";
+            this.lblNoBullets.Size = new System.Drawing.Size(107, 16);
+            this.lblNoBullets.TabIndex = 0;
+            this.lblNoBullets.Text = "No Bullets Left";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SPACE_SHOOTER.Properties.Resources.pozadina;
-            this.ClientSize = new System.Drawing.Size(784, 528);
+            this.ClientSize = new System.Drawing.Size(784, 560);
+            this.Controls.Add(this.lblNoBullets);
             this.MaximumSize = new System.Drawing.Size(800, 600);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.When_Key_Down);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.When_Key_Pressed);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,6 +104,7 @@
         private System.Windows.Forms.Timer tmrEnemies4;
         private System.Windows.Forms.Timer tmrEnemies5;
         private System.Windows.Forms.Timer tmr_my_bullets;
+        private System.Windows.Forms.Label lblNoBullets;
     }
 }
 
